b08ed36c1f9f619fc41a7c1fcfe3576f9e5cf084
Chelle Canale <chel.canale@nuworks.ph>
2023-01-25 16:35:41 +0800

    Updated todo component

--- src/components/TodoApp/TodoApp.jsx
+++ src/components/TodoApp/TodoApp.jsx
@@ -1,16 +1,88 @@
-import { useQuery, useQueryClient } from '@tanstack/react-query'
-import React from 'react'
-import { fetchTodos } from '../../services/jsonPlaceholderAPI'
+import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query'
+import React, { useState } from 'react'
+import { 
+    fetchTodos, 
+    postTodos, 
+    changeStatus,
+    deleteTodo } from '../../services/jsonPlaceholderAPI'
 
 function TodoApp() {
 
     const queryClient = useQueryClient()
-    const todos = useQuery({
-        queryKey: ['todos'],
+    const [todo, setTodo] = useState('')
+    const [pageCount, setPageCount] = useState(1)
+    const [limitCount, setLimitCount] = useState(5)
+
+    const {
+        isLoading,
+        isFetched,
+        isError,
+        error,
+        data: todos
+    } = useQuery({
+        queryKey: ['todos', { page: pageCount, size: limitCount }],
         queryFn: fetchTodos
     })
 
-    console.log('todos', todos)
+    const doneTodoText = "line-through text-green-600"
+    const notDoneButton = "border-green-600 text-green-600 hover:bg-green-600 hover:text-white"
+    const doneButton = "border-gray-600 text-gray-600 hover:bg-gray-600 hover:text-white"
+
+    const handleChange = (e) => {
+        setTodo(e.target.value)
+    }
+
+    const toggleDone = useMutation({
+        mutationFn: changeStatus,
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['todos']
+            })
+        },
+        onError: e => {
+            console.error(e)
+        }
+    })
+
+    const toggleDelete = useMutation({
+        mutationFn: deleteTodo,
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['todos']
+            })
+        },
+        onError: e => {
+            console.error(e)
+        }
+    })
+
+    const handleSubmit = useMutation({
+        mutationFn: postTodos,
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ['todos']
+            })
+            setTodo('')
+        },
+        onError: e => {
+            console.error(e)
+        }
+    })
+
+    const submitData = () => {
+        if(!todo) {
+            return
+        }
+        handleSubmit.mutate({
+            title: todo,
+            completed: false
+        })
+    }
+
+    const handlePaginate = () => {
+        setPageCount(prev => prev + 1)
+        setLimitCount(prev => prev + 5)
+    }
 
     return (
         <>
@@ -19,20 +91,37 @@ function TodoApp() {
                     <h1 className="text-center text-3xl antialiased">Todo List</h1>
                     <div className="mb-4">
                         <div className="flex mt-4">
-                            <input className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Add Todo" />
-                            <button className="flex-no-shrink p-2 border-2 rounded text-teal border-fuchsia-500 hover:text-white hover:text-cyan-900">Add</button>
+                            <input 
+                                className="shadow appearance-none border rounded 
+                                w-full py-2 px-3 mr-4 text-grey-darker" 
+                                placeholder="Add Todo"
+                                onChange={handleChange}
+                                value={todo}
+                            />
+                            <button className="flex-no-shrink p-2 border-2 rounded 
+                                    text-teal border-fuchsia-500 hover:text-white hover:text-cyan-900"
+                                    onClick={ () => submitData() }>Add
+                            </button>
                         </div>
                     </div>
                     <div>
-                        <div className="flex mb-4 items-center">
-                            <p className="w-full text-grey-darkest">Add another component to Tailwind Components</p>
-                            <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-cyan-900 text-green border-green hover:bg-green">Done</button>
-                            <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-cyan-900 hover:bg-red">Remove</button>
-                        </div>
-                        <div className="flex mb-4 items-center">
-                            <p className="w-full line-through text-green">Submit Todo App Component to Tailwind Components</p>
-                            <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-cyan-900 text-grey border-grey hover:bg-grey">Not Done</button>
-                            <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-cyan-900 hover:bg-red">Remove</button>
+
+                        { isFetched && todos.map( data => 
+                            <div className="flex mb-4 items-center" key={data.id}>
+                                <p className={`w-full text-grey-darkest ${ data.completed ? doneTodoText : "" }`}>{ data.title }</p>
+                                <button onClick={() => toggleDone.mutate({ id: data.id, completed: !data.completed })} className={`flex-no-shrink p-2 ml-4 mr-2 border-2 rounded ${ data.completed ? doneButton : notDoneButton } `}>{ !data.completed ? 'Done' : 'Not Done'}</button>
+                                <button onClick={() => toggleDelete.mutate({ id: data.id })} className="flex-no-shrink p-2 ml-2 border-2 border-rose-600 rounded text-red text-rose-600 hover:text-white hover:bg-rose-600">Remove</button>
+                            </div>
+                        )}
+
+                    </div>
+
+                    <div className="mb-4">
+                        <div className="flex mt-4 align-center justify-center">
+                            <button 
+                                className="rounded bg-rose-600 text-white px-4 py-2 mt-5"
+                                onClick={() => handlePaginate() }
+                            >Load More</button>
                         </div>
                     </div>
                 </div>
--- src/services/jsonPlaceholderAPI.js
+++ src/services/jsonPlaceholderAPI.js
@@ -1,12 +1,38 @@
 import axios from 'axios'
 
 const jsonPlaceholderAPI = axios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com/'
+    // baseURL: 'https://jsonplaceholder.typicode.com/'
+    baseURL: 'http://localhost:7000'
 })
 
-export const fetchTodos = async () => {
-    const response = await jsonPlaceholderAPI.get('/todos')
+const config = {
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+    }
+}
+
+export const fetchTodos = async ({ queryKey }) => {
+    const { page, size } = queryKey[1]
+    const response = await jsonPlaceholderAPI.get(`/todos?_page=${page}&_limit=${size}&_sort=id&_order=desc`)
     return response.data;
+}
+
+export const postTodos = async (payload) => {
+    const response = await jsonPlaceholderAPI.post('/todos', payload, config)
+    return response
+}
+
+export const changeStatus = async (payload) => {
+    const { id, completed } = payload
+    const response = await jsonPlaceholderAPI.patch(`/todos/${id}`, { completed }, config)
+    return response
+}
+
+export const deleteTodo = async (payload) => {
+    const { id } = payload
+    const response = await jsonPlaceholderAPI.delete(`/todos/${ id }`)
+    return response
+
 }
 
 export default jsonPlaceholderAPI;
